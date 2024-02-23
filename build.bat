ce6eeef628ae5f725049d555e38359fa8aa47429
Shaw <shawwian@gmail.com>
2024-02-22 23:15:56 -0500

    support embedding images in bmp or png format

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist build\ mkdir build
 pushd build
-cl /nologo /Zi /W4 "%~dp0main.c"
+cl /nologo /Zi /W4 /Femake_icon.exe "%~dp0main.c"
 popd
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <windows.h> // just need BITMAPHEADERINFO but including only wingdi.h doesnt work... lame.
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -9,6 +9,8 @@
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize2.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 #define ARRAY_COUNT(a) (sizeof(a) / sizeof((a)[0]))
 
@@ -21,7 +23,6 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
-#pragma pack(push,1)
 typedef struct {
 	u16 reserved_zero; // must always be zero
 	u16 image_type;    // 1 for icon (.ICO) image, 2 for cursor (.CUR) image. Other values are invalid.
@@ -38,7 +39,6 @@ typedef struct {
 	u32 image_size_in_bytes;
 	u32 offset; // offset of the bmp or png data from the beginning of the ico file
 } IcoEntry;
-#pragma pack(pop)
 
 bool file_write(FILE *f, u8 *data, u64 size) {
 	u64 bytes_written = fwrite(data, 1, size, f);
@@ -47,11 +47,12 @@ bool file_write(FILE *f, u8 *data, u64 size) {
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		printf("Usage: %s png_filename\n", argv[0]);
+		printf("Usage: %s input_image_filename [--png]\n", argv[0]);
 		return 1;
 	}
 
 	char *filename = argv[1];
+	bool mode_png = argc > 2 && 0 == strncmp(argv[2], "--png", 5);
 
 	int og_width, og_height, og_channels;
 	unsigned char* og_image_data = stbi_load(filename, &og_width, &og_height, &og_channels, STBI_rgb_alpha);
@@ -69,72 +70,109 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	// write .ico header
+	//-----------------------------------------------------------------------------
+	// Write .ico header
+	//-----------------------------------------------------------------------------
 	if (!file_write(ico_file, (u8*)&header, sizeof(header))) {
 		fprintf(stderr, "failed to write header to icon.ico\n");
 		return 1;
 	}
 
 	//-----------------------------------------------------------------------------
-	// Write Icon Directory Entries
+	// Generate images of each size
+	//-----------------------------------------------------------------------------
+	u8 *images[ARRAY_COUNT(sizes)] = {0};
+	int image_sizes[ARRAY_COUNT(sizes)] = {0};
+
+	for (i32 i=0; i<num_sizes; ++i) {
+		u32 size = sizes[i];
+		u32 width = size;
+		u32 height = size;
+
+		if (mode_png) {
+			u8 *image_data = stbir_resize_uint8_linear(
+					og_image_data, og_width, og_height, 0,
+					0, size, size, 0,
+					STBIR_RGBA);
+			if (!image_data) {
+				fprintf(stderr, "failed to generate %dx%d image for icon\n", size, size);
+				return 1;
+			}
+			images[i] = stbi_write_png_to_mem(image_data,
+				width * STBI_rgb_alpha, 
+				width, height, STBI_rgb_alpha, 
+				&image_sizes[i]);
+			free(image_data);
+			if (images[i] == 0) {
+				fprintf(stderr, "failed to write png to ico file\n");
+				return 1;
+			}
+
+		} else {
+			// NOTE(shaw): The height of the BMP image must be twice the
+			// height declared in the image directory. The second half of the
+			// bitmap should be an AND mask for the existing screen pixels, with
+			// the output pixels given by the formula 
+			//     Output = (Existing AND Mask) XOR Image
+			// Set the mask to be zero everywhere for a clean overwrite. 
+			height *= 2;
+			u32 resized_num_bytes = width * height * STBI_rgb_alpha;
+			image_sizes[i] = sizeof(BITMAPINFOHEADER) + resized_num_bytes;
+			images[i] = calloc(1, image_sizes[i]);
+
+			BITMAPINFOHEADER *bmi_header = (BITMAPINFOHEADER*)images[i];
+			bmi_header->biSize = sizeof(BITMAPINFOHEADER);
+			bmi_header->biWidth = width;
+			bmi_header->biHeight = height;
+			bmi_header->biPlanes = 1;
+			bmi_header->biBitCount = 32;
+			bmi_header->biCompression = BI_RGB;
+			bmi_header->biSizeImage = resized_num_bytes;
+
+			u8 *resized_image_data = images[i] + sizeof(BITMAPINFOHEADER);
+			u8 *check = stbir_resize_uint8_linear(
+					og_image_data, og_width, og_height, 0,
+					resized_image_data, size, size, 0,
+					STBIR_RGBA);
+			if (!check) {
+				fprintf(stderr, "failed to generate %dx%d image for icon\n", size, size);
+				return 1;
+			}
+		}
+	}
+
+	//-----------------------------------------------------------------------------
+	// Write icon directory entries
 	//-----------------------------------------------------------------------------
 	u32 data_offset = sizeof(IcoHeader) + (num_sizes * sizeof(IcoEntry));
 	for (i32 i=0; i<num_sizes; ++i) {
 		u32 size = sizes[i];
 		u8 entry_size = size == 256 ? 0 : (u8)size;
+
 		IcoEntry entry = {
 			.image_width = entry_size,
 			.image_height = entry_size,
 			.num_colors_in_palette = 0,
 			.color_planes = 1,
 			.bits_per_pixel = 32,
-			.image_size_in_bytes = size * (2*size) * STBI_rgb_alpha,
+			.image_size_in_bytes = image_sizes[i],
 			.offset = data_offset,
 		};
 		if (!file_write(ico_file, (u8*)&entry, sizeof(entry))) {
 			fprintf(stderr, "failed to write image data to file\n");
 			return 1;
 		}
-		data_offset += sizeof(BITMAPINFOHEADER) + entry.image_size_in_bytes;
+		data_offset += image_sizes[i];
 	}
 
 	//-----------------------------------------------------------------------------
-	// Write Image data 
+	// Write image data
 	//-----------------------------------------------------------------------------
 	for (i32 i=0; i<num_sizes; ++i) {
-		u32 size = sizes[i];
-		// NOTE(shaw): The height of the BMP image must be twice the
-		// height declared in the image directory. The second half of the
-		// bitmap should be an AND mask for the existing screen pixels, with
-		// the output pixels given by the formula 
-		//     Output = (Existing AND Mask) XOR Image
-		// Set the mask to be zero everywhere for a clean overwrite. 
-		u32 resized_num_bytes = size * (2*size) * STBI_rgb_alpha;
-
-		BITMAPINFOHEADER bmi_header = {
-			.biSize = sizeof(BITMAPINFOHEADER),
-			.biWidth = size,
-			.biHeight = 2 * size,
-			.biPlanes = 1,
-			.biBitCount = 32,
-			.biCompression = BI_RGB,
-			.biSizeImage = resized_num_bytes,
-		};
-		if (!file_write(ico_file, (u8*)&bmi_header, sizeof(bmi_header))) {
-			fprintf(stderr, "failed to write bitmap info header to file\n");
-			return 1;
-		}
-	
-		u8 *resized_image_data = calloc(1, resized_num_bytes);
-		stbir_resize_uint8_linear(
-			og_image_data, og_width, og_height, 0,
-			resized_image_data, size, size, 0,
-			STBIR_RGBA);
-		if (!file_write(ico_file, resized_image_data, resized_num_bytes)) {
+		if (!file_write(ico_file, images[i], image_sizes[i])) {
 			fprintf(stderr, "failed to write image data to file\n");
 			return 1;
 		}
-		free(resized_image_data);
 	}
 
 	return 0;
